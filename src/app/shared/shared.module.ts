5e03e47d36a07b30d3d9143416f0cb0b4ca9b36f
Danilo Ariza <daniloaq.1229@gmail.com>
2021-07-31 18:25:53 -0500

    Header component added
    - Header base component created (featured card will be added as a separate component)
    - Header component added to home feature template
    - css theme variables added for header
    -

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [NavigationComponent],
-  exports: [NavigationComponent]
+  declarations: [HeaderComponent, NavigationComponent],
+  exports: [HeaderComponent, NavigationComponent]
 })
 export class SharedModule {}
