6ec9b31b30e2910cb62422f25f88656bb9f2e9e7
Danilo Ariza <daniloaq.1229@gmail.com>
2021-07-31 17:40:43 -0500

    Navigation component added
    - Navigation component created
    - icons from font used instead of images
    - css variables added for theme
    - @shared tsconfig custom path added
    - shared module added to home module
    - navigation component added to home feature

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { NavigationComponent } from './components/navigation/navigation.component';
+
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [NavigationComponent],
+  exports: [NavigationComponent]
 })
 export class SharedModule {}
