0979dc29ecfb14b9d031d8f098919a446a2835dc
Danilo Ariza <daniloaq.1229@gmail.com>
2021-07-31 14:14:27 -0500

    Home route added
    - Base home lazy load route added to app routing
    - Base home component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { environment } from '@environments/environment';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/home/home.module').then((m) => m.HomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: environment.useHashRoutes })],
