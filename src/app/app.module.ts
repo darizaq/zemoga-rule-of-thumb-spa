6dc1e1a52cbaf2050beb939f38ee05d7d4f50361
Danilo Ariza <daniloaq.1229@gmail.com>
2021-07-31 14:35:21 -0500

    Translate configuration added
    - @ngx-translate added
    - loader factory created
    - base i18n file added
    - @core custom path added to tsconfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { httpTranslateLoaderFactory } from '@core/factories/http-translate-loader.factory';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule.withServerTransition({ appId: 'serverApp' })],
+  imports: [
+    AppRoutingModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
