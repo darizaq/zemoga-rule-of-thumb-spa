ab043d489db6276a3abfc223b0f8aa4772a8601e
Danilo Ariza <daniloaq.1229@gmail.com>
2021-07-31 14:05:18 -0500

    Base application routing added
    - App routing module added
    - router-outlet added to app template
    - environments tsconfig path added
    - useHash set as false

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
+  imports: [AppRoutingModule, BrowserModule.withServerTransition({ appId: 'serverApp' })],
   providers: [],
   bootstrap: [AppComponent]
 })
