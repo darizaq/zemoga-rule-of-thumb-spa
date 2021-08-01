82365d1a05dfe07c099bf1770994218058ece686
Danilo Ariza <daniloaq.1229@gmail.com>
2021-07-31 22:09:00 -0500

    Banner supplementar component added
    - banner supplemental component created
    - fixes for banner padding and background positioning
    - css variables added to default theme
    - banner supplemental added to home feature template

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,23 @@ import { BannerMainComponent } from './components/banner-main/banner-main.compon
 import { FeaturedCardComponent } from './components/featured-card/featured-card.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { BannerSupplementalComponent } from './components/banner-supplemental/banner-supplemental.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [BannerMainComponent, FeaturedCardComponent, HeaderComponent, NavigationComponent],
-  exports: [BannerMainComponent, FeaturedCardComponent, HeaderComponent, NavigationComponent]
+  declarations: [
+    BannerMainComponent,
+    BannerSupplementalComponent,
+    FeaturedCardComponent,
+    HeaderComponent,
+    NavigationComponent
+  ],
+  exports: [
+    BannerMainComponent,
+    BannerSupplementalComponent,
+    FeaturedCardComponent,
+    HeaderComponent,
+    NavigationComponent
+  ]
 })
 export class SharedModule {}
