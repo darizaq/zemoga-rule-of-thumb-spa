48af3d5be4f708fcbc1de44b37b4f42e6dececeb
Danilo Ariza <daniloaq.1229@gmail.com>
2021-07-31 20:17:21 -0500

    Featured card component added
    - Featured card component created
    - font sizing fixes for description
    - underline style fix for featured card more information link
    - default theme css variables added for featured card
    - featured card component added to header component template
    - featured card sizing and background positioning fixes in header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { FeaturedCardComponent } from './components/featured-card/featured-card.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HeaderComponent, NavigationComponent],
-  exports: [HeaderComponent, NavigationComponent]
+  declarations: [FeaturedCardComponent, HeaderComponent, NavigationComponent],
+  exports: [FeaturedCardComponent, HeaderComponent, NavigationComponent]
 })
 export class SharedModule {}
