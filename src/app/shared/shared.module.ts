a7a769f265ff5c5d15bc80388384d28d546fbd36
Danilo Ariza <daniloaq.1229@gmail.com>
2021-07-31 21:25:24 -0500

    to-rem function and banner main component added
    - to-rem scss function added to calculate values based on pixel values
    - banner main component added
    - font sizing for banner main fixed
    - banner main css variables added to default theme
    - banner component added to home feature template

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { BannerMainComponent } from './components/banner-main/banner-main.component';
 import { FeaturedCardComponent } from './components/featured-card/featured-card.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [FeaturedCardComponent, HeaderComponent, NavigationComponent],
-  exports: [FeaturedCardComponent, HeaderComponent, NavigationComponent]
+  declarations: [BannerMainComponent, FeaturedCardComponent, HeaderComponent, NavigationComponent],
+  exports: [BannerMainComponent, FeaturedCardComponent, HeaderComponent, NavigationComponent]
 })
 export class SharedModule {}
