744c15a4eba8e79ebcfb91df82783949c690f67e
Danilo Ariza <daniloaq.1229@gmail.com>
2021-08-01 21:54:29 -0500

    Env variables exposed on browser side
    - APP_INITIALIZER added on server module to load environment variables from process.env and expose them to browser side with TransferState

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
+import { TransferState } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
+import { envVariablesLoaderFactory } from '@core/factories/env-variables-loader.factory';
 
 @NgModule({
-  imports: [AppModule, ServerModule],
+  imports: [AppModule, ServerModule, ServerTransferStateModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: envVariablesLoaderFactory,
+      deps: [TransferState]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppServerModule {}
