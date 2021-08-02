3188789306611fb948a82e2d95a95572ed021d33
Danilo Ariza <daniloaq.1229@gmail.com>
2021-08-02 00:15:40 -0500

    Rulings list service consumption added
    - rulings list service consumption added on home feature
    - error message integration in home feature

--- src/mocks/rulings-list.mock.ts
+++ src/mocks/rulings-list.mock.ts
@@ -19,3 +19,21 @@ export const mockRulingsListResponse = {
     }
   }
 };
+
+export const mockRulingsList = [
+  {
+    id: 'id',
+    category: 'category',
+    description: 'mock description',
+    lastUpdated: '2021-02-26T23:44:50.326Z',
+    name: 'Name Lastname',
+    photo: {
+      resolution1x: 'resource.png',
+      resolution2x: 'resource@2x.png'
+    },
+    votes: {
+      negative: 2,
+      positive: 2
+    }
+  }
+];
